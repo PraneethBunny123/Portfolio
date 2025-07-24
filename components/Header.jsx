b5b0a698265dc11544de8ce0f270caaedf00e7ec
praneeth <gp.gunturu@gmail.com>
2025-07-24 15:43:50 -0500

    created a nav component and styling header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,11 @@
 import Link from "next/link"
+import Nav from "./Nav"
+import { Button } from "./ui/button"
 
 export default function Header() {
     return (
         <header className="py-8 xl:py-12 text-white">
-            <div className="container mx-auto">
+            <div className="container mx-auto flex justify-between items-center">
                 {/* LOGO */}
                 <Link href='/'>
 					<h1 className='text-4xl font-semibold'>
@@ -11,7 +13,16 @@ export default function Header() {
 					</h1>
 				</Link>
 
-                {/* Desktop Nav */}
+                {/* Desktop Nav and Hire button*/}
+                <div className="hidden xl:flex items-center gap-8">
+                    <Nav />
+                    <Link href='/contact'>
+                        <Button>Hire me</Button>
+                    </Link>
+                </div>
+
+                {/* Mobile Nav*/}
+                <div className="xl:hidden">nobile nav</div>
             </div>
         </header>
     )
---
+++ components/Nav.jsx
@@ -0,0 +1,32 @@
+'use client'
+
+const links = [
+    {
+        name: 'home',
+        path: '/'
+    },
+    {
+        name: 'services',
+        path: '/services'
+    },
+    {
+        name: 'work',
+        path: '/work'
+    },
+    {
+        name: 'resume',
+        path: '/resume'
+    },
+    {
+        name: 'contact',
+        path: '/contact'
+    }
+]
+
+export default function Nav() {
+    return (
+        <nav>
+            Desktop Nav
+        </nav>
+    )
+}
