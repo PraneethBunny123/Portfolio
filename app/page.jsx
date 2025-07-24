492597cd24cc52d73764013dfe84aaaad76fa9e6
praneeth <gp.gunturu@gmail.com>
2025-07-24 18:18:33 -0500

    Completing the home page (root page.js) by adding some styling and static content about myself

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,34 @@
+import { FiDownload } from 'react-icons/fi'
 
-export default function Home() {
-  return (
-    <main className="text-orange-500">Hello world</main>
-  );
+import { Button } from '@/components/ui/button'
+
+const Home = () => {
+	return (
+		<section className='h-full'>
+			<div className='container mx-auto h-full'>
+				<div className='flex flex-col xl:flex-row justify-between items-center xl:pb-24'>
+
+					{/* text */}
+					<div className='text-center xl:text-left order-2 xl:order-none'>
+                        <span className='h3 text-white/100'>Software Developer</span>
+                        <h1 className='h2 text-white/80'>
+                            Hello I'm <br/><span className='text-accent'>Praneeth G</span>
+                        </h1>
+                        
+                        <p className='max-w-[500px] mb-9 text-white/80'>
+                            Results-driven Software Developer with 3+ years of experience building responsive, scalable, 
+                            and user-friendly web & mobile applications, with full-stack capabilities.
+                        </p>
+
+						{/* button and socials */}
+						
+					</div>
+
+					
+				</div>
+			</div>
+		</section>
+	)
 }
+
+export default Home
