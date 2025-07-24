c803940fa094c33436dedd6e2e380fa8d0a02640
praneeth <gp.gunturu@gmail.com>
2025-07-23 19:08:57 -0500

    header logo section

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,18 @@
+import Link from "next/link"
+
 export default function Header() {
     return (
         <header className="py-8 xl:py-12 text-white">
-            <div className="container mx-auto">header</div>
+            <div className="container mx-auto">
+                {/* LOGO */}
+                <Link href='/'>
+					<h1 className='text-4xl font-semibold'>
+						Praneeth G<span className='text-accent'>.</span>
+					</h1>
+				</Link>
+
+                {/* Desktop Nav */}
+            </div>
         </header>
     )
 }
