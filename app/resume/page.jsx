ec60be486cde7d1773adac347468a30be7421a6d
praneeth <gp.gunturu@gmail.com>
2025-07-25 18:44:58 -0500

    Added tabs list and creating unique tab fro each of the resume area

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -126,8 +126,22 @@ const skills = {
 
 export default function Resume() {
     return (
-        <div>
-            resume
-        </div>
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+            }}
+            className="min-h-[80vh] flex justify-center items-center py-12 xl:py-0"
+        >
+            <Tabs>
+                <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+                    <TabsTrigger value="experience"> Experience</TabsTrigger>
+                    <TabsTrigger value="skills">Skills</TabsTrigger>
+                    <TabsTrigger value="education">Education</TabsTrigger>
+                    <TabsTrigger value="about">About me</TabsTrigger>                
+                </TabsList>
+            </Tabs>
+        </motion.div>
     )
 }
