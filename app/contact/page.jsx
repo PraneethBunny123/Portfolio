b19cb0492d0dc5c7f348928c9a889923fa7a8761
praneeth <gp.gunturu@gmail.com>
2025-07-26 13:25:51 -0500

    working on contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,3 +1,31 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { motion } from "framer-motion";
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+import emailjs from "emailjs-com";
+import { useState } from "react";
+
+const info = [
+    {
+        icon: <FaPhoneAlt />,
+        title: "phone",
+        description: "+1 (512)956-8437",
+    },
+    {
+        icon: <FaEnvelope />,
+        title: "email",
+        description: "gpraneeth.dev39@gmail.com",
+    },
+    {
+        icon: <FaMapMarkerAlt />,
+        title: "Address",
+        description: "1515 Ennis Joslin Rd, Corpus Christi, TX-78412",
+    },
+];
+
+
 export default function Contact() {
     return (
         <div>
